562654f143f9a9bc78d50e20ede5cfcec6f128dc
SRS-Employee <SRS-Employee@DESKTOP-RIUOBHR>
2019-06-14 18:43:24 +0530

    Intial push

--- jdbcpractic/src/com/jdbc/practice/BatchUpdate.java
+++ jdbcpractic/src/com/jdbc/practice/BatchUpdate.java
@@ -9,18 +9,18 @@ public class BatchUpdate {
 		try {
 			Class.forName("oracle.jdbc.driver.OracleDriver");
 			Connection con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","system","root");
-			con.setAutoCommit(false);
-			Statement st = con.createStatement();
-		st.addBatch("select * from emp2");
-			st.addBatch("insert into emp2 values('rohit',1001,25000)");
-			st.addBatch("update emp2 set name='rahul' where empid=5001");
-			int res[]= st.executeBatch();
+		//	con.setAutoCommit(false);
+			Statement statement = con.createStatement();
+			//statement.addBatch("select * from emp2");
+			statement.addBatch("insert into emp2 values('rohit',1001,25000)");
+			statement.addBatch("update emp2 set name='rahul' where empid=5001");
+			int res[]= statement.executeBatch();
 			
 			for(int i=1; i<res.length; i++)
 			{
 				System.out.println("Number of record affected "+res[i]);
 			}
-			con.commit();
+			//con.commit();
 			con.close();
 		}
 		catch(Exception e)
